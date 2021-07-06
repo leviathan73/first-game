0296029382b5b8b73b979d66328eeec3ece06630
Krystian <krystianwychowaniec3@gmail.com>
2021-07-06 10:58:49 +0200

    Initialised first game files to the game directory and added the protagonist/player/mainCharacter in our webpage

--- game.js
+++ game.js
@@ -3,3 +3,21 @@ const ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+class Player {
+    constructor(x,y,speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+}
+function drawPlayer() {
+    ctx.beginPath()
+    ctx.fillStyle = "white";
+    ctx.fillRect(canvas.width / 2, canvas.height - 150, 100, 100);
+}
+
+function update() {
+    
+}
+drawPlayer()

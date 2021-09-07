bc8470b3808773c1b456b0950416f19a0785c9b1
Krystian <krystianwychowaniec3@gmail.com>
2021-09-07 08:17:07 +0200

    version with some three.js

--- js/2dmath.js
+++ js/2dmath.js
@@ -1,4 +1,4 @@
-class Point {
+export class Point {
   x = 0;
   y = 0;
   constructor(x, y) {
@@ -14,12 +14,16 @@ class Point {
   }
 }
 
-class Position {
+export class Position {
   x = 0;
   y = 0;
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  inBox(x1, y1, x2, y2) {
+    return this.x >= x1 && this.x <= x2 && this.y <= y2 && this.y >= y1;
   }
 
   vectorTo(p) {
@@ -30,12 +34,16 @@ class Position {
   }
 }
 
-class Vector {
+export class Vector {
   x = 0;
   y = 0;
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  copy() {
+    return new Vector(this.x, this.y);
   }
 
   magnitude() {
@@ -46,6 +54,41 @@ class Vector {
     let normX = this.x / this.magnitude();
     let normY = this.y / this.magnitude();
     return new Vector(normX, normY);
+  }
+
+  normalize2() {
+    let normX = this.x / this.magnitude();
+    let normY = this.y / this.magnitude();
+    this.x = normX;
+    this.Y = normY;
+  }
+
+  setMagnitude(m) {
+    return this.normalize().multiply(m);
+  }
+
+  setMagnitude2(m) {
+    const newV = this.normalize().multiply(m);
+    this.x = newV.x;
+    this.y = newV.y;
+  }
+
+  angle() {
+    return Math.atan2(this.x, -this.y);
+  }
+
+  rotate(beta) {
+    let newX = Math.cos(beta) * this.x - Math.sin(beta) * this.y;
+    let newY = Math.sin(beta) * this.x + Math.cos(beta) * this.y;
+    return new Vector(newX, newY);
+  }
+
+  rotate2(beta) {
+    let newX = Math.cos(beta) * this.x - Math.sin(beta) * this.y;
+    let newY = Math.sin(beta) * this.x + Math.cos(beta) * this.y;
+
+    this.x = newX;
+    this.y = newY;
   }
 
   multiply(by) {
@@ -60,14 +103,12 @@ class Vector {
   }
 }
 
-class Box {
+export class Box {
   x = 0;
-    y = 0;
-    
+  y = 0;
+
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
-
-
 }
